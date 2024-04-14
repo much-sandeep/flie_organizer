a30a77aac493e66a7b15d4f2ed0aa6d2a4b652c6
Sandeep Sharma <sandeepshr1725@gmail.com>
2024-04-14 18:27:55 +0545

    prolly final

--- fileOrganize.py
+++ fileOrganize.py
@@ -1,8 +1,22 @@
 import os
 import shutil
-path=r"C:\Users\sharm\Downloads"
-a=shutil.disk_usage(path)
-fileList=os.listdir(path)
-for i in fileList:
-    base_name, extension = os.path.splitext(i)
-    print(f"File Name: {i}, Extension: {extension}")
+
+source = r"C:\Users\sharm\Downloads"
+file_list = os.listdir(source)
+
+for file_name in file_list:
+    file_path = os.path.join(source, file_name)
+    
+    if os.path.isfile(file_path):
+        base_name, extension = os.path.splitext(file_name)
+        print(f"Extension: {extension}")
+        destination_dir = os.path.join(source, extension.strip('.').lower())
+
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+
+        shutil.move(file_path, os.path.join(destination_dir, file_name)) 
+        print(f"Moved {file_name} to {destination_dir}")
+
+    else:
+        print(f"{file_name} is not a regular file")
